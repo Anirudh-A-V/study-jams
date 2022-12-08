d8d94cc2797484a429c75f079ddfa82d23ac8f88
Anirudh A V <anirudh.av02@gmail.com>
2022-12-08 20:48:15 +0530

    React Study Jams : Day 3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import {Heading,Flex} from "@chakra-ui/react"
+import React, {useState} from 'react';
+import { Heading, Flex, Text, Input, Button } from "@chakra-ui/react"
+import { AiOutlineSearch } from "react-icons/ai";
 
-export const Navbar = ()=>{
+export const Navbar = ({Name}) => {
+    const [text, setText] = useState('');
+    console.log(text);
+
     return (
-       <Flex bg="grey" h="100px" align="center" justifyContent="space-between" p="6">
-           <Heading color="white">Cart</Heading>
-           <Heading>👜</Heading>
-       </Flex>
+        <Flex bg="#FFF" h="100px" align="center" justifyContent="center" p="6">
+
+            <Heading color="green">{Name}</Heading>
+
+            <Flex marginLeft={'50px'}>
+                <Input variant='outline' placeholder='Outline' maxW={'400px'} minW={'300px'} onChange={(e) => setText(e.target.value)} />
+                <Button  bg={'green'} color={'white'}>
+                    <AiOutlineSearch />
+                </Button>
+            </Flex>
+
+            <Flex marginLeft={'50px'}>
+                <Flex flexDirection={'column'}>
+                    <Text color={'green'}>No. of items : 0</Text>
+                    <Text color={'green'}>Sub Total : 0</Text>
+                </Flex>
+                <Heading>👜</Heading>
+            </Flex>
+
+        </Flex>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Box, Flex, SimpleGrid } from "@chakra-ui/react"
+import React, { useState } from 'react'
+import { Flex, SimpleGrid, Input } from "@chakra-ui/react"
 import { Navbar } from "../components/Navbar/Navbar"
 import Cards from '../components/Cards/Cards'
 
@@ -13,13 +13,21 @@ export const Home = () => {
     'Sai',
     'Sanjay',
   ]
+  
+  const [query, setQuery] = useState('')
+
+  function search(items){
+    return items.filter((name) => name.toLowerCase().includes(query.toLowerCase()))
+  }
 
   return (
-    <Flex flexDirection={'column'}>
-      <Navbar />
-      <SimpleGrid spacing={1} templateColumns='repeat(4, 1fr)'  paddingLeft={'200px'} paddingRight={'200px'}>
+    <Flex flexDirection={'column'} bg={'#F5F5F5'}>
+      <Navbar Name={'Veggy'} />
 
-        {Name.map((item, index) => {
+      <Input variant='outline' placeholder='Outline' onChange={(e) => setQuery(e.target.value)} />
+
+      <SimpleGrid spacing={1} templateColumns='repeat(4, 1fr)'  paddingLeft={'200px'} paddingRight={'200px'} paddingTop={'50px'} paddingBottom={'50px'}>
+        {search(Name).map((item, index) => {
           return (
             <Cards Name={item}  key={index} />
           )
